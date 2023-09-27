ad3fef92a6329d539872d6ab60fe6b0c50b91dae
thiago-arend <thiago.arend@gmail.com>
2023-09-27 01:45:58 -0300

    exercicio 06

---
+++ aula-06/tests/app.test.ts
@@ -0,0 +1,22 @@
+import supertest from "supertest";
+
+import app from "./../src/app";
+
+const api = supertest(app);
+
+describe("API test", () => {
+  it("GET /event should return status 200", async () => {
+    const result = await api.get("/event");
+    expect(result.statusCode).toBe(200);
+  });
+
+  it("GET /event should return object as described", async () => {
+    const result = await api.get("/event");
+    expect(result.body).toEqual({
+      id: expect.any(Number),
+      title: "Super Event!",
+      image: "https://img.freepik.com/fotos-gratis/publico-animado-assistindo-fogos-de-artificio-de-confete-e-se-divertindo-no-festival-de-musica-a-noite-copiar-espaco_637285-559.jpg",
+      date: "2023-07-21T00:00:00.000Z"
+    });
+  });
+});
